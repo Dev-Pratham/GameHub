7e73a395ce46c13bd5824519b0780b6c224761d7
Dev-Pratham <pratham.chaurasia31@gmail.com>
2025-03-20 22:22:49 +0530

    building game cards

---
+++ src/components/GameCard.tsx
@@ -0,0 +1,22 @@
+import { Card, Image } from "@chakra-ui/react";
+
+import { Game } from "../hooks/UseGames";
+
+interface Props {
+  game: Game;
+}
+
+const GameCard = ({ game }: Props) => {
+  return (
+    <Card.Root maxW="sm" overflow="hidden" borderRadius={10}>
+      <Image src={game.background_image} alt={game.name} />
+      <Card.Body gap="2">
+        <Card.Title fontSize="2xl">{game.name}</Card.Title>
+        {/* <Card.Description>{game.description}</Card.Description> */}
+      </Card.Body>
+      <Card.Footer gap="2"></Card.Footer>
+    </Card.Root>
+  );
+};
+
+export default GameCard;
--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -1,5 +1,6 @@
-import { Text } from "@chakra-ui/react";
+import { SimpleGrid, Text } from "@chakra-ui/react";
 import UseGames from "../hooks/UseGames";
+import GameCard from "./GameCard";
 
 const GameGrid = () => {
   //custom hook for seperaation of concerns
@@ -8,11 +9,11 @@ const GameGrid = () => {
   return (
     <>
       {error && <Text>{error}</Text>}
-      <ul>
+      <SimpleGrid columns={{ sm: 1, md: 2, lg: 3 }} gap={10} padding={5}>
         {games.map((game) => (
-          <li key={game.id}>{game.name}</li>
+          <GameCard key={game.id} game={game} />
         ))}
-      </ul>
+      </SimpleGrid>
     </>
   );
 };
