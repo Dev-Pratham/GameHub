211143d84950ad48a4219d8a50869b40bfea4c0e
Dev-Pratham <pratham.chaurasia31@gmail.com>
2025-06-06 17:49:08 +0545

    Imported bunch of icons and created iconMap to map icon for each platfrom

--- src/components/PlatformIconList.tsx
+++ src/components/PlatformIconList.tsx
@@ -1,15 +1,44 @@
 import { Platform } from "../hooks/UseGames";
-import { Text } from "@chakra-ui/react";
+import { HStack, Text } from "@chakra-ui/react";
+import {} from "react-icons/fa";
+import {
+  FaWindows,
+  FaPlaystation,
+  FaXbox,
+  FaApple,
+  FaLinux,
+} from "react-icons/fa";
+import { SiNintendo } from "react-icons/si";
+import { Icon } from "@chakra-ui/react";
+import { FaAndroid, FaGlobe } from "react-icons/fa";
+import { IconType } from "react-icons";
+
 interface Props {
   parent_platforms: { platform: Platform }[];
 }
 
 const PlatformIconList = ({ parent_platforms }: Props) => {
+  const iconMap: { [key: string]: IconType } = {
+    pc: FaWindows,
+    playstation: FaPlaystation,
+    xbox: FaXbox,
+    nintendo: SiNintendo,
+    ios: FaApple,
+    linux: FaLinux,
+    android: FaAndroid,
+    web: FaGlobe,
+  };
+
   return (
     <>
-      {parent_platforms.map(({ platform }) => (
-        <Text key={platform.id}>{platform.name}</Text>
-      ))}
+      <HStack>
+        {parent_platforms.map(({ platform }) => {
+          // return <Text>{platform.platform.slug}</Text>
+          //agar nahi hai toh return null warna application crach hojaiga
+          if (!iconMap[platform.slug]) return null;
+          return <Icon as={iconMap[platform.slug]} />;
+        })}
+      </HStack>
     </>
   );
 };
