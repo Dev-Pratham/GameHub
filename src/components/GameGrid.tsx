9b41da8f5d90fcbc096e31acf44e64cb1eb65e86
Dev-Pratham <pratham.chaurasia31@gmail.com>
2025-06-11 23:14:36 +0545

    package update and making skeleton grid

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -1,13 +1,25 @@
 import { SimpleGrid, Text } from "@chakra-ui/react";
 import UseGames from "../hooks/UseGames";
 import GameCard from "./GameCard";
+import GameCardSkeleton from "./GameCardSkeleton";
 
 const GameGrid = () => {
   //custom hook for seperaation of concerns
-  const { games, error } = UseGames();
+  const { games, error, loading } = UseGames();
 
   return (
     <>
+      {loading && (
+        <SimpleGrid
+          columns={{ sm: 1, md: 2, lg: 3, xl: 5 }}
+          padding={5}
+          gap={10}
+        >
+          {Array.from({ length: 20 }).map((_, index) => (
+            <GameCardSkeleton key={index} />
+          ))}
+        </SimpleGrid>
+      )}
       {error && <Text>{error}</Text>}
       <SimpleGrid columns={{ sm: 1, md: 2, lg: 3, xl: 5 }} gap={10} padding={5}>
         {games.map((game) => (
