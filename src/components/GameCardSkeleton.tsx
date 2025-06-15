a07885cff2590727a7558e1bcfc4670685491bfc
Dev-Pratham <pratham.chaurasia31@gmail.com>
2025-06-15 22:14:48 +0545

    added explanation for unusual behaviour

--- src/components/GameCardSkeleton.tsx
+++ src/components/GameCardSkeleton.tsx
@@ -3,7 +3,9 @@ import { Card, Skeleton, SkeletonText } from "@chakra-ui/react";
 const GameCardSkeleton = () => {
   return (
     // The width value is arbitrary, adjust as needed
-    <Card.Root width={"100%"}>
+    // In this case the width is set to 100% to fill the parent container
+    // This is a skeleton card component that mimics the structure of the GameCard
+    <Card.Root width="100%">
       <Card.Header />
       {/* The height value is arbitrary  */}
       <Skeleton height="200px" />
