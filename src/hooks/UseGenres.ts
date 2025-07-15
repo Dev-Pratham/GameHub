5667ed043416ea95039dc0222cd5bb7cba289af7
Dev-Pratham <pratham.chaurasia31@gmail.com>
2025-07-15 18:11:12 +0530

    shipped genres with the app making the app faster as genres doesnot changes quickly

--- src/hooks/UseGenres.ts
+++ src/hooks/UseGenres.ts
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import apiClient from "../services/api-client";
+import genres from "@/data/genres";
 
 export interface Genres {
   id: number;
@@ -13,30 +14,33 @@ interface FetchGenresResponse {
   results: Genres[];
 }
 const UseGenres = () => {
-  const [genres, setGeners] = useState<Genres[]>([]);
-  const [error, setError] = useState("");
-  const [loading, setLoading] = useState(false);
+  // const [genres, setGeners] = useState<Genres[]>([]);
+  // const [error, setError] = useState("");
+  // const [loading, setLoading] = useState(false);
 
-  useEffect(() => {
-    setLoading(true);
-    const controller = new AbortController();
-    apiClient
-      .get<FetchGenresResponse>("/genres", { signal: controller.signal })
-      .then((response) => {
-        setLoading(false);
-        setGeners(response.data.results);
-      })
-      .catch((error) => {
-        if (error instanceof Error) return;
-        setError(error.message);
-        setLoading(false);
-      });
-    ``;
+  // useEffect(() => {
+  //   setLoading(true);
+  //   const controller = new AbortController();
+  //   apiClient
+  //     .get<FetchGenresResponse>("/genres", { signal: controller.signal })
+  //     .then((response) => {
+  //       setLoading(false);
+  //       setGeners(response.data.results);
+  //     })
+  //     .catch((error) => {
+  //       if (error instanceof Error) return;
+  //       setError(error.message);
+  //       setLoading(false);
+  //     });
+  //   ``;
 
-    return () => controller.abort();
-  }, []);
+  //   return () => controller.abort();
+  // }, []);
 
-  return { genres, error, loading };
+  // return { genres, error, loading };
+  //This is for static data
+  //We will not use the apiClient here
+  return { genres: genres, error: null, loading: false };
 };
 
 export default UseGenres;
